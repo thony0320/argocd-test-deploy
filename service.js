d3e4871c7efb78ed591845be0fd9271784b9c3ef
thony0320 <stuff.93@gmail.com>
2024-12-24 06:05:18 -0500

    service update refresh buttom

--- service.js
+++ service.js
@@ -10,7 +10,25 @@ app.get('/health', (req, res) => {
   res.status(200);
 });
 
+// app.get('/', (req, res) => {
+//   const name = process.env.NAME || 'World';
+//   res.send(`Hello ${name}!`);
+// });
+
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Refresh Button</title>
+    </head>
+    <body>
+      <h1>Hello ${name}!</h1>
+      <button onclick="window.location.reload();">Refrescar</button>
+    </body>
+    </html>
+  `);
 });
