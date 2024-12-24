cc5f70a1ea47b07dc55907b7634df38732fceb8a
thony0320 <stuff.93@gmail.com>
2024-12-24 06:11:43 -0500

    test service update

--- service.js
+++ service.js
@@ -10,25 +10,25 @@ app.get('/health', (req, res) => {
   res.status(200);
 });
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
-});
-
 // app.get('/', (req, res) => {
 //   const name = process.env.NAME || 'World';
-//   res.send(`
-//     <!DOCTYPE html>
-//     <html lang="en">
-//     <head>
-//       <meta charset="UTF-8">
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//       <title>Refresh Button</title>
-//     </head>
-//     <body>
-//       <h1>Hello ${name}!</h1>
-//       <button onclick="window.location.reload();">Refrescar</button>
-//     </body>
-//     </html>
-//   `);
+//   res.send(`Hello ${name}!`);
 // });
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Refresh Button</title>
+    </head>
+    <body>
+      <h1>Hello ${name}!</h1>
+      <button onclick="window.location.reload();">Refrescar</button>
+    </body>
+    </html>
+  `);
+});
