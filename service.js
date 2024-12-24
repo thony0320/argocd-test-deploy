84915c6441c1a104e6d12cb244c1cef0afd0f4df
thony0320 <stuff.93@gmail.com>
2024-12-24 06:23:28 -0500

    Service update

--- service.js
+++ service.js
@@ -15,23 +15,23 @@ app.get('/health', (req, res) => {
 //   res.send(`Hello ${name}!`);
 // });
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Thony';
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Refresh Button</title>
-    </head>
-    <body>
-      <h1>Hello ${name}!</h1>
-      <button onclick="window.location.reload();">Refrescar</button>
-    </body>
-    </html>
-  `);
-});
+// app.get('/', (req, res) => {
+//   const name = process.env.NAME || 'Thony';
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <title>Refresh Button</title>
+//     </head>
+//     <body>
+//       <h1>Hello ${name}!</h1>
+//       <button onclick="window.location.reload();">Refrescar</button>
+//     </body>
+//     </html>
+//   `);
+// });
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'Thony';
