a11d243be85c1950ac6efaa5954f48192321a78a
thony0320 <stuff.93@gmail.com>
2024-12-24 10:20:13 -0500

    Update service java script

--- service.js
+++ service.js
@@ -10,10 +10,10 @@ app.get('/health', (req, res) => {
   res.status(200);
 });
 
-// app.get('/', (req, res) => {
-//   const name = process.env.NAME || 'World';
-//   res.send(`Hello ${name}!`);
-// });
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'Thony';
+  res.send(`Hello ${name}!`);
+});
 
 // app.get('/', (req, res) => {
 //   const name = process.env.NAME || 'Thony';
@@ -33,22 +33,22 @@ app.get('/health', (req, res) => {
 //   `);
 // });
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Thony';
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Refresh and Ve a Health Thony</title>
-    </head>
-    <body>
-      <h1>Hello ${name}!</h1>
-      <button onclick="window.location.reload();">Refrescar</button>
-      <br><br>
-      <button onclick="window.location.href='/health';">Ir a Health</button>
-    </body>
-    </html>
-  `);
-});
+// app.get('/', (req, res) => {
+//   const name = process.env.NAME || 'Thony';
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <title>Refresh and Ve a Health Thony</title>
+//     </head>
+//     <body>
+//       <h1>Hello ${name}!</h1>
+//       <button onclick="window.location.reload();">Refrescar</button>
+//       <br><br>
+//       <button onclick="window.location.href='/health';">Ir a Health</button>
+//     </body>
+//     </html>
+//   `);
+// });
